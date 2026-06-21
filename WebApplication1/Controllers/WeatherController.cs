cc630cb3b55190f90017569498a04234854be332
Gihadelrwiny <gihadelrwiny2004@gmail.com>
2026-06-21 19:51:38 +0300

    make without bonus

---
+++ WebApplication1/Controllers/WeatherController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly List<WeatherData> WeatherList = new()
+        {
+            new WeatherData
+            {
+                City = "Cairo",
+                Temperature = 35
+            },
+            new WeatherData
+            {
+                City = "Alexandria",
+                Temperature = 29
+            },
+            new WeatherData
+            {
+                City = "Aswan",
+                Temperature = 40
+            }
+        };
+        [HttpGet("Weather")]
+        public IActionResult GetAll()
+        {
+            return Ok(WeatherList);
+        }
+        [HttpGet("{city}")]
+        public IActionResult GetCity(string city)
+        {
+            var cityget = WeatherList.FirstOrDefault(w => w.City.Equals(city, StringComparison.OrdinalIgnoreCase));
+            if (cityget is null) return NotFound();
+            return Ok(cityget);
+        }
+    }
+}
---
+++ WebApplication1/Models/WeatherData.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class WeatherData
+    {
+        public string City { get; set; }
+        public int Temperature { get; set; }
+    }
+}
