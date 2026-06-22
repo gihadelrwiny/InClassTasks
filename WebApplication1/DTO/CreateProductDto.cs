735137b2fa3735ad0134469c4dc1f3443396f48a
Gihadelrwiny <gihadelrwiny2004@gmail.com>
2026-06-22 19:19:36 +0300

    without bonus

---
+++ WebApplication1/DTO/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTO
+{
+    public class CreateProductDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Range(10,1000)]
+        public decimal Price { get; set; }
+    }
+}
--- WebApplication1/Models/WeatherData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class WeatherData
-    {
-        public string City { get; set; }
-        public int Temperature { get; set; }
-    }
-}
